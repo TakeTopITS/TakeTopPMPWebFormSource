cbf59c70accbe90f727dd7c8ef4a0bad0c781804
TakeTopITS Group <service@taketopits.com>
2026-03-13 12:01:33 +0800

    web

--- Web/App_Code/ShareClass.cs
+++ Web/App_Code/ShareClass.cs
@@ -59,7 +59,7 @@ public static class ShareClass
         //
     }
 
-    public static string SystemVersionID = "V2026.3.12";
+    public static string SystemVersionID = "V2026.3.13";
 
     public static string SystemLatestLoginUser = "";
     public static string SystemDBer = "";
